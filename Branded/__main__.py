6f75f17ab863aa537d342088a27aae90f3d64092
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2025-03-07 10:25:16 +0530

    Update __main__.py

--- Branded/__main__.py
+++ Branded/__main__.py
@@ -34,7 +34,7 @@ async def main():
     await run_async_inline()
     logs.info("Successfully Deployed !!")
     await run_async_calls()
-    logs.info("Do Visit - @pallavi_sarkar2)
+    logs.info("Do Visit - @pallavi_sarkar2")
     await idle()
 
 
